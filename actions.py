06f56fb4f39f1b3e0b4425f7dbc5413c24c2ab6a
Luck Wei <luckwei123@gmail.com>
2022-05-04 01:24:22 +1000

    in actions.py updated functions: dash, claim_divinities, craft_rage

--- actions.py
+++ actions.py
@@ -15,24 +15,18 @@ def craft_rage(screen):
     """
     Quickly craft and use the rage pill
     """
-    names_iter = (
-        ("craft_button", 0.2),
-        ("temporary_craft", 0.1),
-        ("down_scroll", 0.05),
-        ("down_scroll", 0.1),
-        ("craft_rage_pill", 0.1),
-        ("rage_button", 0.1)
-    )
-
-    click_iter(coords_iter_from_names(screen, names_iter))
+    click_iter(coords_iter_from_names(screen, [
+        ("craft_button", 0.2), ("temporary_craft", 0.1),
+        ("down_scroll", 0.05), ("down_scroll", 0.1),
+        ("craft_rage_pill", 0.1), ("rage_button", 0.1)
+    ]))
 
 def dash(screen, sleeptime=None):
     """
     Dash if button is ready, based on color comparison
     """
-
-    #NULL CONDITION
-    if pixel(*coords[screen]["dash_end"]) in ((4, 22, 48), (4, 23, 50)):
+    #NULL CONDITION: Last part of dash button is different to complete dash button
+    if pixel(*coords[screen]["dash_end"]) != pixel(*coords[screen]["dash_button"]):
         return
 
     win32api.keybd_event(key_code["d"], 0, 0, 0)
@@ -45,10 +39,49 @@ def claim_divinities(screen):
     """
     Claims points and sends minions on trips when ready
     """
-    ascension_before = pixel(*coords[screen]["ascension_tab"])
-    sleep(0.1)
-    ascension_after = pixel(*coords[screen]["ascension_tab"])
-
-    #NULL CONDITION
-    if ascension_before == ascension_after or pixel(*coords[screen]["close_ascension"]) == (255, 255, 255):
+    ascension_before = pixel(*coords[screen]["ascension_tab"]) #sample ascension
+    
+    #NULL CONDITION: menu is out (white close button is present)
+    if pixel(*coords[screen]["close_ascension"]) == (255, 255, 255):
         return
+
+    sleep(0.05)
+    ascension_after = pixel(*coords[screen]["ascension_tab"]) #sample ascension again
+
+    #NULL CONDITION: ascension tab is not blinking
+    if ascension_before == ascension_after:
+        return
+
+    click_iter(coords_iter_from_names(screen, [
+        ("ascension_tab", 0.2), ("skilltree_tab", 0.1)
+    ]))
+
+    minions_before = pixel(*coords[screen]["minions_tab"]) #sample minion
+    sleep(0.1)
+    minions_after = pixel(*coords[screen]["minions_tab"]) #sample minion again
+
+    #NULL CONDITION: minions tab is not blinking
+    if minions_before == minions_after:
+        click(coords[screen]["close_tab"])
+        return
+    
+    click(coords[screen]["minions_tab"], 0.1)
+
+    #CHECK if daily activated
+    if pixel(*coords[screen]["daily"]) == (255, 255, 255):
+        send_minions = "send_minions2"
+    else:
+        send_minions = "send_minions"
+    
+    click_iter(coords_iter_from_names(screen, [
+        (send_minions, 0.1), (send_minions, 0.1), ("close_tab", 0.2)
+    ]))
+    
+    
+
+
+
+
+
+
+    
