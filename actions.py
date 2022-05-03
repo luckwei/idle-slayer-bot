f8f531c37f556ae4234d7015b36a5fd48a15126d
Luck Wei <luckwei123@gmail.com>
2022-05-04 00:41:54 +1000

    Add documentation for functions in devices/screen.py & actions.py

--- actions.py
+++ actions.py
@@ -12,19 +12,29 @@ from devices.keyboard import key_code
 from gameplay.coords import coords, coords_iter_from_names
 
 def craft_rage(screen):
+    """
+    Quickly craft and use the rage pill
+    """
     names_iter = (
         ("craft_button", 0.2),
         ("temporary_craft", 0.1),
         ("down_scroll", 0.05),
         ("down_scroll", 0.1),
         ("craft_rage_pill", 0.1),
-        ("rage_button", 0.1),
+        ("rage_button", 0.1)
     )
+
     click_iter(coords_iter_from_names(screen, names_iter))
 
 def dash(screen, sleeptime=None):
+    """
+    Dash if button is ready, based on color comparison
+    """
+
+    #NULL CONDITION
     if pixel(*coords[screen]["dash_end"]) in ((4, 22, 48), (4, 23, 50)):
         return
+
     win32api.keybd_event(key_code["d"], 0, 0, 0)
     win32api.keybd_event(key_code["d"], 0, win32con.KEYEVENTF_KEYUP, 0)
 
@@ -32,9 +42,13 @@ def dash(screen, sleeptime=None):
         sleep(sleeptime)
 
 def claim_divinities(screen):
+    """
+    Claims points and sends minions on trips when ready
+    """
     ascension_before = pixel(*coords[screen]["ascension_tab"])
     sleep(0.1)
     ascension_after = pixel(*coords[screen]["ascension_tab"])
 
+    #NULL CONDITION
     if ascension_before == ascension_after or pixel(*coords[screen]["close_ascension"]) == (255, 255, 255):
         return
--- devices/screen.py
+++ devices/screen.py
@@ -2,12 +2,20 @@
 import win32api
 
 def detect_screen():
-    screen_width = win32api.GetSystemMetrics(78)
+    """
+    Categorise screen based on combined virtual width of monitor(s)
+    """
+
+    screen_width = win32api.GetSystemMetrics(78) #width of virtual screen
+
+    #my dual monitor setup: 1280x720* | 1920x1080
     if screen_width == 3200:
-        return "side" #1280x720* | 1920x1080
-    elif screen_width in (1920, 1536):
-        return "large" #1920x1080*
-    else:
-        #unknown monitor
-        print(f"MONITOR NOT RECOGNISED, INFO: {screen_width}")
-        return "unknown"
+        return "side"
+    
+    #my single monitor setups: 1920x1080* or 1536x864*
+    elif screen_width == 1920 or screen_width == 1536:
+        return "large"
+
+    #unknown monitor
+    print(f"MONITOR NOT RECOGNISED, INFO: {screen_width}")
+    return "unknown"
