3ce5fbc7d252a7954e0bb041b727886fb9ac0ff0
Luck Wei <luckwei123@gmail.com>
2022-05-04 00:49:22 +1000

    Change comments of devices/screen.py to start capitalised

--- devices/screen.py
+++ devices/screen.py
@@ -6,16 +6,16 @@ def detect_screen():
     Categorise screen based on combined virtual width of monitor(s)
     """
 
-    screen_width = win32api.GetSystemMetrics(78) #width of virtual screen
+    screen_width = win32api.GetSystemMetrics(78) #Width of virtual screen
 
-    #my dual monitor setup: 1280x720* | 1920x1080
+    #My dual monitor setup: 1280x720* | 1920x1080
     if screen_width == 3200:
         return "side"
     
-    #my single monitor setups: 1920x1080* or 1536x864*
+    #My single monitor setups: 1920x1080* or 1536x864*
     elif screen_width == 1920 or screen_width == 1536:
         return "large"
 
-    #unknown monitor
+    #Unknown monitor
     print(f"MONITOR NOT RECOGNISED, INFO: {screen_width}")
     return "unknown"
